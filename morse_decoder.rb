7bdc65de2d60f6c5fb9eb1c819c2c9424385576c
Besufekad Alemu <besurye@gmail.com>
2023-05-17 17:56:54 +0300

    first solution with debugging

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,4 +1,4 @@
-MORSE_DIC = {
+@MORSE_DIC = {
   '.-' => 'A',
   '-...' => 'B',
   '-.-.' => 'C',
@@ -26,3 +26,30 @@ MORSE_DIC = {
   '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
+
+def process_char(letter)
+  # @ is immutable symbol used as referencing method names
+  @this_letter = @MORSE_DIC[letter]
+  @this_letter
+end
+
+def word_decoder(word)
+ @letters = word.split
+ @current_word = ''
+ @letters.each do |letter|
+  process_char(letter)
+  @current_word += process_char(letter)
+ end
+ print "#{@current_word}"
+end
+
+def msg_decoder(message)
+  @words = message.split('   ')
+  @words.each do |word|
+    word_decoder(word)
+  end
+end
+
+
+
+msg_decoder('-- -.--   -. .- -- .')
